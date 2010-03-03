7e7a9184e8d575108d4eefc81ecd952fb787c8ed
Jonas <jonas@Monsterburken.(none)>
2010-03-03 22:05:35 +0100

    bild och post klassen

---
+++ src/com/forum/client/Post.java
@@ -0,0 +1,15 @@
+package com.forum.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.smartgwt.client.widgets.Canvas;
+
+public class Post extends Canvas{
+
+	public Post() {
+		super();
+		
+	}
+
+	
+	
+}
